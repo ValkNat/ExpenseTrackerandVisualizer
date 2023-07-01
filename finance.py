9f89c0a2b24abf0db876368b1d04e68ae67c05a8
Natalie <132787852+ValkNat@users.noreply.github.com>
2023-07-01 15:51:14 -0500

    V2.0 - allows appending to csv file

--- finance.py
+++ finance.py
@@ -7,6 +7,7 @@ import numpy as np
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from tkcalendar import Calendar, DateEntry
 import csv
+import os
 
 #tkinter initialization + setup
 t = Tk()
@@ -20,11 +21,12 @@ expenses = []
 def addExpense():
     title = simpledialog.askstring("Title of expense", "Title of expense?")
     cost = simpledialog.askinteger("Cost of expense:", "Cost of expense?")
-    date = simpledialog.askinteger("Date of expense:", "Date of expense?")
+    date = simpledialog.askstring("Date of expense:", "Date of expense?")
     expense = {"title": title, "cost": cost, "date": date}
     expenses.append(expense)
     expense_list.insert(0, expense)
 
+#updates or initializes the pie-chart
 def update_pie_chart():
     costs=[expense['cost'] for expense in expenses]
     titles = [expense['title'] for expense in expenses]
@@ -35,14 +37,20 @@ def update_pie_chart():
     pie_canvas.draw()
     pie_canvas.get_tk_widget().pack()
 
-#saves the expenses to a CSV file
+#saves the expenses to a CSV file - checks if file already exists and creates if it doesn't
 def save_to_csv():
     keys = expenses[0].keys()
-
-    with open('expenses.csv', 'w', newline='') as output_file:
-        dict_writer = csv.DictWriter(output_file, keys)
-        dict_writer.writeheader()
-        dict_writer.writerows(expenses)
+    try:
+        with open('expenses.csv', 'r', newline='') as output_file:
+         if os.path.exists('expenses.csv'):
+              with open('expenses.csv', 'a', newline='') as output_file:
+                  dict_writer = csv.DictWriter(output_file, keys)
+                  dict_writer.writerows(expenses) 
+    except FileNotFoundError:
+             with open('expenses.csv', 'w', newline='') as output_file:
+                 dict_writer = csv.DictWriter(output_file, keys)
+                 dict_writer.writeheader()
+                 dict_writer.writerows(expenses)
 
 #tkinter widget initialization + setup
 add_expense_button = tk.Button(text = "Add Expense", command=addExpense)
